b45f5c6636e42aa43fe8ac788dbad2bbd65945fa
questionmark5482 <jiahaichenxju@163.com>
2023-09-27 22:00:03 +0800

    Add some sounds

--- Scripts/Player_state_machine.gd
+++ Scripts/Player_state_machine.gd
@@ -13,6 +13,7 @@ var current_substate: String
 
 const STATE_RUN = "run"
 const STATE_DANCE = "dance"
+const SUBSTATE_IDLE = "idle"
 const SUBSTATE_ATTACK = "attack"
 const SUBSTATE_DEFEND = "defend"
 const SUBSTATE_DODGE = "dodge"
@@ -47,15 +48,18 @@ func handle_dance_state(delta):
 			handle_dodge_substate(delta)
 
 func handle_attack_substate(delta):
-	# Implement attack logic here
+	print("Dance_Attack!")
+	current_substate = SUBSTATE_IDLE
 	pass
 
 func handle_defend_substate(delta):
-	# Implement defend logic here
+	print("Dance_Defend!")
+	current_substate = SUBSTATE_IDLE
 	pass
 
 func handle_dodge_substate(delta):
-	# Implement dodge logic here
+	print("Dance_dodge!")
+	current_substate = SUBSTATE_IDLE
 	pass
 
 func _input(event):
@@ -69,11 +73,13 @@ func handle_run_state_input(event):
 	if event.is_action_pressed("dance"):
 		print("Enter Dance State")
 		current_state = STATE_DANCE
-		current_substate = SUBSTATE_ATTACK
+		current_substate = SUBSTATE_IDLE
 
 func handle_dance_state_input(event):
 	# Problem is here. Attack state can ony detect attack.
 	match current_substate:
+		SUBSTATE_IDLE:
+			handle_idle_substate_input(event)
 		SUBSTATE_ATTACK:
 			handle_attack_substate_input(event)
 		SUBSTATE_DEFEND:
@@ -81,17 +87,19 @@ func handle_dance_state_input(event):
 		SUBSTATE_DODGE:
 			handle_dodge_substate_input(event)
 
-func handle_attack_substate_input(event):
+func handle_idle_substate_input(event):
 	if event.is_action_pressed("attack"):
-		print("Dance_Attack!")
-		current_substate = SUBSTATE_DEFEND
-
-func handle_defend_substate_input(event):
+		current_substate = SUBSTATE_ATTACK
 	if event.is_action_pressed("defend"):
-		print("Dance_Defend!")
+		current_substate = SUBSTATE_DEFEND
+	if event.is_action_pressed("dodge"):
 		current_substate = SUBSTATE_DODGE
 
+func handle_attack_substate_input(event):
+	pass
+
+func handle_defend_substate_input(event):
+	pass
+
 func handle_dodge_substate_input(event):
-	if event.is_action_pressed("dodge"):
-		print("Dance_dodge!")
-		current_state = STATE_RUN
+	pass
