4ee34ed82c72784b0b5885225c29e56c5c525498
questionmark5482 <jiahaichenxju@163.com>
2023-10-02 22:18:23 +0800

    2023/10/12

---
+++ Scripts/monster.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const MONSTER_DRAG = SPEED * 0.2
+const STEER_VELOCITY = 1
+
+var player_vector: Vector2
+var player_direction: Vector2
+var move_direction: Vector2
+var facing_angle: float
+
+var current_state: String
+
+const STATE_CHASE= "chase"
+const STATE_IDLE = "idle"
+
+var audio_player: AudioStreamPlayer
+
+var  player
+
+func _ready():
+	current_state = STATE_CHASE
+	audio_player = get_node("AudioStreamPlayer")
+	player = get_parent().get_node("Player")
+#	print(player.transform.origin)
+
+func _physics_process(delta):
+	match current_state:
+		STATE_CHASE:
+			handle_chase_state(delta)
+		STATE_IDLE:
+			handle_idle_state(delta)
+
+func handle_chase_state(delta):
+	player_vector = player.get_global_transform().origin - self.get_global_transform().origin
+	player_direction = player_vector.normalized()
+
+	velocity = player_direction * SPEED
+	facing_angle = player_direction.angle()
+	set_global_rotation(facing_angle + PI/2)
+
+	velocity = velocity.move_toward(Vector2(0, 0), MONSTER_DRAG)
+	move_and_slide()
+	
+func handle_idle_state(delta):
+	pass
+
+
